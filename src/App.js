2d5292277eeaed2643467a97b5cd20d190ba266e
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-01 18:12:55 -0500

    Added header and footer to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Header from './components/Header';
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route />
+        </Routes>
+        <Footer />
+      </Router>
+      
       
     </div>
   );
