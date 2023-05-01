5d41c8bf7cf88073f9253bcf57e08b2067780887
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-01 18:12:20 -0500

    Fixed indentation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -4,12 +4,12 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
   return (
     <nav>
-      <ul>
-        <li><Link to="/">About me</Link> </li>
-        <li><Link to="/portfolio">Portfolio</Link> </li>
-        <li><Link to="/contact">Contact</Link> </li>
-        <li><Link to="/resume">Resume</Link> </li>
-      </ul>
+        <ul>
+            <li><Link to="/">About me</Link> </li>
+            <li><Link to="/portfolio">Portfolio</Link> </li>
+            <li><Link to="/contact">Contact</Link> </li>
+            <li><Link to="/resume">Resume</Link> </li>
+        </ul>
     </nav>
   )
 }
