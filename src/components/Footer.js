caf0b07a997052b97045c9adc379136cf239f050
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-01 18:00:35 -0500

    Added footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faGitHubSquare, faLinkedIn, faTwitterSquare } from '@fortawesome/free-solid-svg-icons';
+// import { faGithubSquare } from '@fortawesome/free-solid-svg-icons'
+
+
+export default function Footer() {
+  return (
+    <footer>
+        <ul>
+            <li><a href="https://github.com/BiaJorgensen">GitHub</a></li>
+            <li><a href="https://www.linkedin.com/in/beatriz-bia-jorgensen-840a41182/">LinkedIn</a></li>
+            <li><a href="https://twitter.com/BiaJorgensen">Twitter</a></li> 
+        </ul>
+    </footer>
+  )
+}
