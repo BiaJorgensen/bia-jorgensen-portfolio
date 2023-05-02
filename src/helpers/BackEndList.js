d4920d407e7361a1a60954693e54d153bdd2c424
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-02 11:08:34 -0500

    Created helpr with list of backend proficiencies

---
+++ src/helpers/BackEndList.js
@@ -0,0 +1,8 @@
+export const backEndList = [
+    'Node.js (Server-side programming language)',
+    'Express.js',
+    'MySQL',
+    'MongoDB',
+    'Sequelize',
+    'RESTful APIs (API design and development)'
+]
