c83540aa4879ede39a6628f30815f2641101b4e0
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-01 22:27:30 -0500

    Added react-icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,14 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faGitHubSquare, faLinkedIn, faTwitterSquare } from '@fortawesome/free-solid-svg-icons';
-// import { faGithubSquare } from '@fortawesome/free-solid-svg-icons'
+import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs';
 
 
 export default function Footer() {
   return (
     <footer>
         <ul>
-            <li><a href="https://github.com/BiaJorgensen">GitHub</a></li>
-            <li><a href="https://www.linkedin.com/in/beatriz-bia-jorgensen-840a41182/">LinkedIn</a></li>
-            <li><a href="https://twitter.com/BiaJorgensen">Twitter</a></li> 
+            <li><a href="https://github.com/BiaJorgensen"><BsGithub /></a></li>
+            <li><a href="https://www.linkedin.com/in/beatriz-bia-jorgensen-840a41182/"><BsLinkedin /></a></li>
+            <li><a href="https://twitter.com/BiaJorgensen"><BsTwitter /></a></li> 
         </ul>
     </footer>
   )
