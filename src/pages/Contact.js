07f1d13c1980677b8757a82bb5b7fb01a74f5c20
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-02 09:56:52 -0500

    Modified form to set value to blank after email is sent; Added useState and variables to assist

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,29 +1,44 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import emailjs from '@emailjs/browser';
 
 // Code assistance from https://www.emailjs.com/docs/examples/reactjs/
 export default function Contact() {
     const form = useRef();
+    // Variables to initiate input as blank
+    const [nameInput, setNameInput] = useState('');
+    const [emailInput, setEmailInput] = useState('');
+    const [messageInput, setMessageInput] = useState('');
+
+    // Set input to value that user is adding
+    const nameChange = (e) => {setNameInput(e.target.value)};
+    const emailChange = (e) => {setEmailInput(e.target.value)};
+    const messageChange = (e) => {setMessageInput(e.target.value)};
   
     const sendEmail = (e) => {
         e.preventDefault();
+        // After email is sent, input is changed back to blank
+        setNameInput('');
+        setEmailInput('');
+        setMessageInput('');
+
         emailjs.sendForm('service_ohjj2qc', 'template_wz07h7k', form.current, 'FFCUtIU5y7i-GJuov')
         .then((result) => {
             console.log(result.text);
         }, (error) => {
             console.log(error.text);
         });
-        };
+    };
+    
 
   return (
     <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
-        <input type="text" name="user_name" />
+        <input type="text" name="user_name" value={nameInput} onChange={nameChange}/>
         <label>Email address</label>
-        <input type="email" name="user_email" />
+        <input type="email" name="user_email" value={emailInput} onChange={emailChange}/>
         <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
+        <input type="text" name="message" value={messageInput} onChange={messageChange}/>
+        <button type="submit">Send</button>
     </form>
   )
 }
