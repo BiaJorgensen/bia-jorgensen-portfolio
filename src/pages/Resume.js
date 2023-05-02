737f1104c7adc105bc79abd6b8f376b1c207207f
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-02 11:10:31 -0500

    Created Resume page; Using frontend and backendhelper lists; Added downloadable resume pdf

---
+++ src/pages/Resume.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import CV from '../assets/bia-jorgensen-cv.pdf';
+import { frontEndList } from '../helpers/FrontEndList';
+import { backEndList } from '../helpers/BackEndList';
+
+export default function Resume() {
+  return (
+    <div>
+      <h2>Resume</h2>
+      <h3>Download my <Link to={CV} target="_blank" download>resume</Link></h3>
+      <h4>Front-End Proficiencies</h4>
+      <ul>
+        {/* Mapping though list to populate all items in the array without repeating <li> */}
+        {frontEndList.map(item => (
+           <li>{item}</li> 
+        ))}
+      </ul>
+      <h4>Back-End Proficiencies</h4>
+      <ul>
+        {/* Mapping though list to populate all items in the array without repeating <li> */}
+        {backEndList.map(item => (
+           <li>{item}</li> 
+        ))}
+      </ul>
+    </div>
+  )
+}
