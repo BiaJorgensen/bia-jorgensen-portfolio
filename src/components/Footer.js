f0a7b917a38cc0f327408c03be9805a0059d92bf
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-01 22:30:17 -0500

    Added code to open links in new tab

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,9 @@ export default function Footer() {
   return (
     <footer>
         <ul>
-            <li><a href="https://github.com/BiaJorgensen"><BsGithub /></a></li>
-            <li><a href="https://www.linkedin.com/in/beatriz-bia-jorgensen-840a41182/"><BsLinkedin /></a></li>
-            <li><a href="https://twitter.com/BiaJorgensen"><BsTwitter /></a></li> 
+            <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/BiaJorgensen"><BsGithub /></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/beatriz-bia-jorgensen-840a41182/"><BsLinkedin /></a></li>
+            <li><a target="_blank" rel="noopener noreferrer" href="https://twitter.com/BiaJorgensen"><BsTwitter /></a></li> 
         </ul>
     </footer>
   )
