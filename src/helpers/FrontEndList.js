dbf761276b8d71194ffdbb430455d665f027cc16
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-02 10:59:59 -0500

    Created helper file with array of front-end proficiencies

---
+++ src/helpers/FrontEndList.js
@@ -0,0 +1,15 @@
+export const frontEndList = [
+    'JavaScript',
+    'HTML',
+    'CSS',
+    'Bootstrap',
+    'jQuery',
+    'React',
+    'Responsive Design',
+    'Git (Version Control System)',
+    'Webpack (Front-end build tool)',
+    'Jest (Testing framework)',
+    'Chrome DevTools (Debugging tool)',
+    'RESTful APIs (API integration)',
+    'Progressive Web Apps (PWA) development'
+]
