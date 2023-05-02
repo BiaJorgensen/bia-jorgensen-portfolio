0bd41014cc541a1697d1aff21678b7cc0e35cf80
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-02 08:15:08 -0500

    Created array with Project's info

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,46 @@
+import Scheduler from '../assets/images/scheduler-logo.png';
+import EventFinder from '../assets/images/event-finder.PNG';
+import WeatherApp from '../assets/images/weather-app.PNG';
+import Planner from '../assets/images/daily-planner.PNG';
+import TextEditor from '../assets/images/text-editor.PNG';
+import Quiz from '../assets/images/quiz.PNG';
+
+
+export const projectList = [
+    {
+        title: 'Takepoint Dentistry',
+        image: Scheduler,
+        deployedApp: 'https://appointment-scheduler2.herokuapp.com/',
+        githubRepo: 'https://github.com/BiaJorgensen/scheduler'
+    },
+    {
+        title: 'Event Finder',
+        image: EventFinder,
+        deployedApp: 'https://micheala-h.github.io/Event-finder/',
+        githubRepo: 'https://github.com/Micheala-H/Event-finder'
+    },
+    {
+        title: 'Weather App',
+        image: WeatherApp,
+        deployedApp: 'https://biajorgensen.github.io/weather-app/',
+        githubRepo: 'https://github.com/BiaJorgensen/weather-app'
+    },
+    {
+        title: 'Daily Planner',
+        image: Planner,
+        deployedApp: 'https://biajorgensen.github.io/daily-planner/',
+        githubRepo: 'https://github.com/BiaJorgensen/daily-planner'
+    },
+    {
+        title: 'Text Editor',
+        image: TextEditor,
+        deployedApp: 'https://bia-jorgensen-text-editor.herokuapp.com/',
+        githubRepo: 'https://github.com/BiaJorgensen/text-editor-pwa'
+    },
+    {
+        title: 'Quiz',
+        image: Quiz,
+        deployedApp: 'https://biajorgensen.github.io/quiz/',
+        githubRepo: 'https://github.com/BiaJorgensen/quiz'
+    },
+]
