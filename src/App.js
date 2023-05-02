4d31c839ecced4916b6c36dbb881a0db8b1aecaf
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-02 11:12:22 -0500

    Added Route to pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Header from './components/Header';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
 
 function App() {
   return (
@@ -10,7 +14,10 @@ function App() {
       <Router>
         <Header />
         <Routes>
-          <Route />
+          <Route path='/' element={<AboutMe />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/resume' element={<Resume />} />
         </Routes>
         <Footer />
       </Router>
