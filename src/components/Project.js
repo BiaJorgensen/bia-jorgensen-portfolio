18a510e441a1c5965c346570f79bc738bea6b4c0
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-04 17:04:09 -0500

    Modified div to use Card from react-bootstrap; Importing CSS file

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { BsGithub } from 'react-icons/bs';
+import Card from 'react-bootstrap/Card';
+import '../styles/Project.css'
 
 export default function Project({ title, image, deployedApp, githubRepo}) {
   return (
-    <div>
-      <h2>{title}</h2>
-      <img src={image} alt={title} />
-      <div>
-        <a href={deployedApp}>{title}</a>
-        <a href={githubRepo}><BsGithub /></a>
+    <Card>
+      <Card.Img src={image} alt={title} className="image"/>
+      <div className="links">
+        <Card.Link href={deployedApp}>{title} Website </Card.Link>
+        <Card.Link href={githubRepo}><BsGithub /></Card.Link>
       </div>
-    </div>
+    </Card>
   )
 }
