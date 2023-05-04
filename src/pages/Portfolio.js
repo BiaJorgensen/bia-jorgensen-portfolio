10d1b04f0d06c9d8f3c07bb9bf2a473356375f8f
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-04 17:03:29 -0500

    Added container, row and column from react-bootstrap

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Project from '.././components/Project'
 import { projectList } from '../helpers/ProjectList';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export default function Portfolio() {
   return (
-    <div>
+    <Container>
       <h2>My Projects</h2>
+      <Row xs={1} sm={2} md={3}>
       {projectList.map(project => (
-        <Project key={project.title} {...project} />
+        <Col key={project.title} className="mb-3">
+        <Project  {...project} />
+        </Col>
       ))}
-    </div>
+      </Row>
+    </Container>
   )
 }
