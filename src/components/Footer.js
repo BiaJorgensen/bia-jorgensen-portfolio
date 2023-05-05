c405e0f9d619616d37619ce6cc1f552faba02db8
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-04 21:48:10 -0500

    Added styling to footer and icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs';
-
+import '../styles/Footer.css'
 
 export default function Footer() {
   return (
-    <footer>
-        <ul>
-            <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/BiaJorgensen"><BsGithub /></a></li>
-            <li><a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/beatriz-bia-jorgensen-840a41182/"><BsLinkedin /></a></li>
-            <li><a target="_blank" rel="noopener noreferrer" href="https://twitter.com/BiaJorgensen"><BsTwitter /></a></li> 
-        </ul>
+    <footer className="footer">
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/BiaJorgensen"><BsGithub className="icon me-3" /></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/beatriz-bia-jorgensen-840a41182/"><BsLinkedin className="icon me-3" /></a>
+      <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/BiaJorgensen"><BsTwitter className="icon" /></a>   
     </footer>
   )
 }
