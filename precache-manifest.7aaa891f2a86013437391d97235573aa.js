b13f946fac163d0327fdfffe831a600565220d5c
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-06 16:47:02 -0500

    Updates

---
+++ precache-manifest.7aaa891f2a86013437391d97235573aa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c28c7482d87f2e79c99",
+    "url": "/bia-jorgensen-portfolio/static/css/main.a5c234c1.chunk.css"
+  },
+  {
+    "revision": "5c28c7482d87f2e79c99",
+    "url": "/bia-jorgensen-portfolio/static/js/main.5c28c748.chunk.js"
+  },
+  {
+    "revision": "89f8c12492339e6065e3",
+    "url": "/bia-jorgensen-portfolio/static/js/1.89f8c124.chunk.js"
+  },
+  {
+    "revision": "6236fad9d9c7521959ba",
+    "url": "/bia-jorgensen-portfolio/static/css/2.3ba4f36f.chunk.css"
+  },
+  {
+    "revision": "6236fad9d9c7521959ba",
+    "url": "/bia-jorgensen-portfolio/static/js/2.6236fad9.chunk.js"
+  },
+  {
+    "revision": "5349676f0721cabf346b",
+    "url": "/bia-jorgensen-portfolio/static/js/runtime~main.5349676f.js"
+  },
+  {
+    "revision": "e568898789ecffe52495a642ec8e7d2e",
+    "url": "/bia-jorgensen-portfolio/static/media/bia-jorgensen.e5688987.png"
+  },
+  {
+    "revision": "2d643c42798b29faed336ed024261b8f",
+    "url": "/bia-jorgensen-portfolio/static/media/scheduler-logo.2d643c42.png"
+  },
+  {
+    "revision": "2a133df81793b733b114601bde411ef5",
+    "url": "/bia-jorgensen-portfolio/static/media/event-finder.2a133df8.PNG"
+  },
+  {
+    "revision": "cf2234d3db548643eb7e01f4e833b1a5",
+    "url": "/bia-jorgensen-portfolio/static/media/weather-app.cf2234d3.PNG"
+  },
+  {
+    "revision": "0bbefa44a9ebbe734520ef95cd9b96a0",
+    "url": "/bia-jorgensen-portfolio/static/media/daily-planner.0bbefa44.PNG"
+  },
+  {
+    "revision": "5656e1e8d637b4e51c8fd1f9b0b50b92",
+    "url": "/bia-jorgensen-portfolio/static/media/text-editor.5656e1e8.PNG"
+  },
+  {
+    "revision": "bdebe841e3b73e6cefc1e2e039a98cf8",
+    "url": "/bia-jorgensen-portfolio/static/media/quiz.bdebe841.PNG"
+  },
+  {
+    "revision": "a6e0295853d938e9b10d7ec3adc1933a",
+    "url": "/bia-jorgensen-portfolio/static/media/bia-jorgensen-cv.a6e02958.pdf"
+  },
+  {
+    "revision": "fb6a2add606915a066a083943fd54d66",
+    "url": "/bia-jorgensen-portfolio/index.html"
+  }
+];
