e16fe017a281bdab43940f094167f0bb104d7359
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-05-06 17:26:19 -0500

    Added wild card to redirect to main page

--- src/App.js
+++ src/App.js
@@ -16,10 +16,10 @@ function App() {
         <Header />
         <div className="container">
         <Routes>
-          <Route path='/' element={<AboutMe />} />
           <Route path='/portfolio' element={<Portfolio />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/resume' element={<Resume />} />
+          <Route path='*' element={<AboutMe />} />
         </Routes>
         </div>
         <Footer />
