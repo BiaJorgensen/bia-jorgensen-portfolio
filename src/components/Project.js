479bcaf130ac08399df45845b66da1eb17909f21
Bia Jorgensen <BiaJorgensen@users.noreply.github.com>
2023-08-05 17:45:07 -0500

    Changed github icon size

--- src/components/Project.js
+++ src/components/Project.js
@@ -8,8 +8,8 @@ export default function Project({ title, image, deployedApp, githubRepo}) {
     <Card>
       <Card.Img src={image} alt={title} className="image"/>
       <div className="links">
-        <Card.Link target="_blank" rel="noopener noreferrer" className="link" href={deployedApp}>{title} Website </Card.Link>
-        <Card.Link target="_blank" rel="noopener noreferrer" className="link" href={githubRepo}><BsGithub size={60} /></Card.Link>
+        <Card.Link target="_blank" rel="noopener noreferrer" className="link" href={deployedApp}>Website </Card.Link>
+        <Card.Link target="_blank" rel="noopener noreferrer" className="link" href={githubRepo}><BsGithub size={20} /></Card.Link>
       </div>
     </Card>
   )
